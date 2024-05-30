dd2589997659f3928bee2dcf577a97d965fa65d7
dean-daryl <deandaryl7@gmail.com>
2024-05-30 20:52:57 +0200

    authentication ui

--- frontend/src/components/auth/AuthLayout.jsx
+++ frontend/src/components/auth/AuthLayout.jsx
@@ -3,13 +3,26 @@ import Navbar from '../Navbar';
 import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
-  return (
-    <div className="auth-layout">
-      <main className="auth-main-container">
-        <Outlet />
-      </main>
+return (
+    <div className="w-full flex max-md:flex-wrap justify-between">
+        <div className="w-full h-[100vh] bg-[#9ccb3b] flex px-2">
+            <div className="flex flex-col justify-center mx-auto">
+                <p className="text-white text-xl text-center font-sans mx-8">
+                    Have you joined the movement to take your organisation to Zero Waste
+                    in the near future? To best ensure success for your new or improved
+                    recycling project (whether at the office or within your school),
+                    Postwink aims to make this easier for you. See below for our service
+                    offerings.
+                </p>
+            </div>
+        </div>
+        <div className="w-full flex px-2 max-md:mt-5">
+            <div className="flex flex-col justify-center mx-auto">
+                <Outlet />
+            </div>
+        </div>
     </div>
-  );
+);
 };
 
 export default AuthLayout;
--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 
 function Register() {
-  return (
-    <div className='font-3xl underline'>Register</div>
-  )
+   const inputStyle =
+     'flex h-9 w-[300px] rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent ';
+   return (
+     <div>
+       <div className="grid gap-6 ">
+         <p>
+           <span className="text-2xl font-bold">Sign Up</span>
+           <p>
+                <span className="text-sm text-gray-500"> Start your amazing journey</span>
+           </p>
+         </p>
+         <form className="">
+           <div className="grid gap-2">
+             <div className="grid gap-1">
+               <p>Name:</p>
+               <input className={inputStyle} placeholder="Enter your name" />
+               <p>Email:</p>
+               <input className={inputStyle} placeholder="Enter your email" />
+               <p>Password :</p>
+               <input
+                 className={inputStyle}
+                 placeholder="Create a password"
+                 type="password"
+               />
+               <p>Confirm password :</p>
+               <input
+                 className={inputStyle}
+                 placeholder="Confirm your password"
+                 type="password"
+               />
+             </div>
+             <div>
+               <button className="w-[300px] h-9 bg-[#9ccb3b] text-white rounded-md mt-4">
+                 Get started
+               </button>
+             </div>
+           </div>
+         </form>
+         <div className="relative">
+           <div className="absolute inset-0 flex items-center">
+             <span className="w-full border-t" />
+           </div>
+         </div>
+         <div>
+           <p className="text-center">
+             Already have an account?{' '}
+             <a href="/login" className="text-[#9ccb3b]">
+               Log in
+             </a>
+           </p>
+         </div>
+       </div>
+     </div>
+   );
 }
 
 export default Register
