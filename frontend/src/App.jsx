8da6c9d744a7e7c335e4cdbee1ca663ca61b34d8
dean-daryl <deandaryl7@gmail.com>
2024-05-30 14:16:41 +0200

    setup router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+  const routes = [
+    {
+      path: '/',
+      element: <AuthLayout />,
+      children: [{ path: 'register', element: <Register /> },{path:"login", element:<Login />}],
+    },
+  ];
+  const router = (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
   );
+
+  return <></>;
 }
 
-export default App
+export default App;
