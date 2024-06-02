32ad042f6a52d9c0c6ce2864ec4487804dddfbf3
lscblack <l.christian@alustudent.com>
2024-06-02 17:18:32 +0200

    done with dashboard steps

--- frontend/src/components/Dashboard/SideBar.jsx
+++ frontend/src/components/Dashboard/SideBar.jsx
@@ -1,38 +1,73 @@
-import React from 'react';
-import TodayTime from '@mui/icons-material/Today';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents'; 
-import RecyclingIcon from '@mui/icons-material/Recycling';
-import LogoutIcon from '@mui/icons-material/Logout';
-import SettingsIcon from '@mui/icons-material/Settings';
+import React from "react";
+import TodayTime from "@mui/icons-material/Today";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import RecyclingIcon from "@mui/icons-material/Recycling";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SettingsIcon from "@mui/icons-material/Settings";
+import HomeIcon from "@mui/icons-material/Home";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import CancelIcon from "@mui/icons-material/Cancel";
 
-const components = {
-    'Schedule': TodayTime,
-    'Achievements': EmojiEventsIcon,
-    'Recycle': RecyclingIcon,
-    'Logout': LogoutIcon,
-    'Settings': SettingsIcon,
-}
+export const SideBar = ({ isOpen, toggleSidebar }) => {
+  const houseHoldSidebarItems = [
+    { label: "Schedule", icon: <TodayTime /> },
+    { label: "Achievements", icon: <EmojiEventsIcon /> },
+    { label: "Recycle", icon: <RecyclingIcon /> },
+  ];
+  const settings = [
+    { label: "Settings", icon: <SettingsIcon /> },
+    { label: "Logout", icon: <LogoutIcon /> },
+  ];
 
-export const SideBar = () => {
-    const houseHoldSidebarItems = ['Schedule', 'Achievements', 'Recycle', 'Settings', 'Logout'];
-    
-    const handleItemClick = (item) => {
-        setSelectedItem(item);
-    };
-
-    return (
-        <div className='flex flex-col w-1/4 p-2 bg-[green] h-[100vh] text-white'>
-            <h1 className='text-[2rem]'>Dashboard</h1>
-            <hr />
-            {houseHoldSidebarItems.map((item) => {
-                const IconComponent = components[item];
-                return (
-                    <a key={item} onClick={() => handleItemClick(item)} className="flex items-center gap-2 cursor-pointer p-6 hover:underline hover:text-[lightgreen]">
-                        {IconComponent && <IconComponent sx={{ fontSize: '40px' }} />}
-                        <span>{item}</span>
-                    </a>
-                );
-            })}
+  return (
+    <>
+      <div className="sticky top-0 bg-[#207855] text-white py-4 px-4 flex items-center justify-between">
+        <h3 className="text-xl font-bold">ECO-TRACK.RW</h3>
+        <button
+          className="text-white rounded-md focus:outline-none "
+          name="btn"
+          id="btn"
+          onClick={toggleSidebar}
+        >
+          {isOpen ? (
+            <CancelIcon style={{ fontSize: "30px" }} />
+          ) : (
+            <MenuOpenIcon style={{ fontSize: "30px" }} />
+          )}
+        </button>
+      </div>
+      <div className="menu">
+        <div className="py-3">
+          <h3 className="text-primary p-3 font-semibold">Menu</h3>
+          <div className="text-primary">
+            {houseHoldSidebarItems.map((item, index) => (
+              <div
+                key={index}
+                className="flex items-center py-2 px-4 hover:bg-gray-200 cursor-pointer"
+              >
+                <div className="mr-3">{item.icon}</div>
+                <span>{item.label}</span>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      </div>
+      <div className="menu">
+        <div className="py-3">
+          <h3 className="text-primary p-3 font-semibold">Management</h3>
+          <div className="text-primary">
+            {settings.map((item, index) => (
+              <div
+                key={index}
+                className="flex items-center py-2 px-4 hover:bg-gray-200 cursor-pointer"
+              >
+                <div className="mr-3">{item.icon}</div>
+                <span>{item.label}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
