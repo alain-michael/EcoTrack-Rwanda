8cd09952ca62890d96284244726e5121dfad3077
lscblack <l.christian@alustudent.com>
2024-06-04 13:36:36 +0200

    done with redux setup

--- frontend/src/features/SharedDataSlice/SharedData.js
+++ frontend/src/features/SharedDataSlice/SharedData.js
@@ -2,27 +2,31 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // Default values for shared host
 const initialState = {
-    usersLogin: [],
+  usersLogin: [],
+  selectedItem: "Dashboard",
 };
 
 const sharedDataSlice = createSlice({
-    name: 'Eco_rw',
-    initialState,
-    reducers: {
-        addUserLogin: (state, action) => {
-            const users = action.payload;
-            state.usersLogin = users;
-        },
-        resetStateToDefault: (state, action) => {
-            // Reset state to initial values
-            // Object.assign(state, initialState);
-            state.usersLogin = []
-        }
+  name: "Eco_rw",
+  initialState,
+  reducers: {
+    addUserLogin: (state, action) => {
+      const users = action.payload;
+      state.usersLogin = users;
+    },
+    setSelectedItem: (state, action) => {
+      state.selectedItem = action.payload;
+    },
 
-
-    }
+    resetStateToDefault: (state, action) => {
+      // Reset state to initial values
+      // Object.assign(state, initialState);
+      state.usersLogin = [];
+      state.selectedItem = "Dashboard";
+    },
+  },
 });
 
-export const { addUserLogin } = sharedDataSlice.actions;
+export const { addUserLogin, setSelectedItem } = sharedDataSlice.actions;
 
 export default sharedDataSlice.reducer;
