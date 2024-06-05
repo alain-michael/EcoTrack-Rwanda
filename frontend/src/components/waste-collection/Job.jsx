47b712d7918c7815cef256dbb36deae054366630
dean-daryl <deandaryl7@gmail.com>
2024-06-05 16:28:54 +0200

    tomtom map

--- frontend/src/components/waste-collection/Job.jsx
+++ frontend/src/components/waste-collection/Job.jsx
@@ -1,89 +1,28 @@
 import { useState, useMemo, useCallback, useRef, useEffect } from 'react';
-import {
-  GoogleMap,
-  DirectionsRenderer,
-  useJsApiLoader,
-  DistanceMatrixService,
-} from '@react-google-maps/api';
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
+import * as ttmaps from "@tomtom-international/web-sdk-maps";
 
 function Job() {
-  const [directions, setDirections] = useState();
-  const mapRef = useRef();
-  const center = useMemo(
-    () => ({ lat: -1.939826787816454, lng: 30.0445426438232 }),
-    [],
-  );
-  const origin = {
-    lat: -1.939826787816454,
-    lng: 30.0445426438232,
-  };
-  const destination = {
-    lat: -1.9365670876910166,
-    lng: 30.13020167024439,
-  };
-  const options = useMemo(
-    () => ({
-      disableDefaultUI: true,
-      clickableIcons: true,
-    }),
-    [],
-  );
-  const onLoad = useCallback((map) => (mapRef.current = map), []);
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.VITE_API_KEY,
-  });
+  const mapElement = useRef();
+  const [mapZoom, setMapZoom] = useState(8);
+  const [map, setMap] = useState({});
 
-  if (isLoaded) {
-    const service = new google.maps.DirectionsService();
-    service.route(
-      {
-        origin: origin,
-        destination: destination,
-        travelMode: google.maps.TravelMode.DRIVING,
-      },
-      (result, status) => {
-        if (result) {
-          setDirections(result);
-        }
-      },
-    );
+  useEffect(() => {
+    let map = ttmaps.map({
+      key: 'F2xMu3bjLFhJ7BZW3hVE7EpRXT2tblMs',
+      container: mapElement.current,
+      center: [-1.939826787816454, 30.0445426438232],
+      zoom: mapZoom,
+    });
+    setMap(map);
+    return () => map.remove();
+  }, []);
 
-    return (
-      <>
-        <div className="">
-          <GoogleMap
-            zoom={8}
-            center={center}
-            mapContainerClassName="map-container"
-            options={options}
-            onLoad={onLoad}
-          >
-            {directions && (
-              <DirectionsRenderer
-                directions={directions}
-                options={{
-                  polylineOptions: {
-                    zIndex: 50,
-                    strokeColor: '#1976D2',
-                    strokeWeight: 5,
-                  },
-                }}
-              />
-            )}
-            <DistanceMatrixService
-              options={{
-                destinations: [destination],
-                origins: [origin],
-                travelMode: google.maps.TravelMode.DRIVING,
-              }}
-            />
-          </GoogleMap>
-        </div>
-      </>
-    );
-  } else {
-    return <>.... Loading</>;
-  }
+  return (
+    <div className="w-full h-full">
+      <div ref={mapElement} className="h-[100vh]"></div>
+    </div>
+  );
 }
 
 export default Job;
