45a86e9ee8ed385b973c6357f41367e58eb29cd3
dean-daryl <deandaryl7@gmail.com>
2024-06-06 14:07:37 +0200

    swith back to google maps api

--- frontend/src/components/waste-collection/Job.jsx
+++ frontend/src/components/waste-collection/Job.jsx
@@ -1,28 +1,134 @@
 import { useState, useMemo, useCallback, useRef, useEffect } from 'react';
-import "@tomtom-international/web-sdk-maps/dist/maps.css";
-import * as ttmaps from "@tomtom-international/web-sdk-maps";
+import {
+  GoogleMap,
+  DirectionsRenderer,
+  useJsApiLoader,
+  DistanceMatrixService,
+} from '@react-google-maps/api';
 
-function Job() {
-  const mapElement = useRef();
-  const [mapZoom, setMapZoom] = useState(8);
-  const [map, setMap] = useState({});
-
-  useEffect(() => {
-    let map = ttmaps.map({
-      key: 'F2xMu3bjLFhJ7BZW3hVE7EpRXT2tblMs',
-      container: mapElement.current,
-      center: [-1.939826787816454, 30.0445426438232],
-      zoom: mapZoom,
-    });
-    setMap(map);
-    return () => map.remove();
-  }, []);
-
-  return (
-    <div className="w-full h-full">
-      <div ref={mapElement} className="h-[100vh]"></div>
-    </div>
+const Job = () => {
+  const [currentStop, setCurrentStop] = useState(0);
+  const [time, setTime] = useState();
+  const [distance, setDistance] = useState();
+  const [stopName, setStopName] = useState();
+  const [directions, setDirections] = useState();
+  const [origin, setOrigin] = useState({});
+  const mapRef = useRef();
+  const center = useMemo(
+    () => ({ lat: -1.939826787816454, lng: 30.0445426438232 }),
+    [],
   );
-}
+
+  const destination = useMemo(
+    () => ({ lat: -1.939826787816454, lng: 30.0445426438232 }),
+    [],
+  );
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      console.log(lat, lng);
+      setOrigin({ lat, lng });
+    });
+  };
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+  const options = useMemo(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+    }),
+    [],
+  );
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_API_KEY,
+  });
+
+  if (isLoaded) {
+    const service = new google.maps.DirectionsService();
+    service.route(
+      {
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (result) {
+          setDirections(result);
+        }
+      },
+    );
+
+    return (
+      <>
+        <GoogleMap
+          zoom={8}
+          center={center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        >
+          {directions && (
+            <DirectionsRenderer
+              directions={directions}
+              options={{
+                polylineOptions: {
+                  zIndex: 50,
+                  strokeColor: '#1976D2',
+                  strokeWeight: 5,
+                },
+              }}
+            />
+          )}
+          {/* <DistanceMatrixService
+            options={{
+              destinations: [
+                {
+                  location: new google.maps.LatLng(
+                    -1.9355377074007851,
+                    30.060163829002217,
+                  ),
+                },
+                {
+                  location: new google.maps.LatLng(
+                    -1.9358808342336546,
+                    30.08024820994666,
+                  ),
+                },
+                {
+                  location: new google.maps.LatLng(
+                    -1.9489196023037583,
+                    30.092607828989397,
+                  ),
+                },
+                {
+                  location: new google.maps.LatLng(
+                    -1.9592132952818164,
+                    30.106684061788073,
+                  ),
+                },
+                {
+                  location: new google.maps.LatLng(
+                    -1.9487480402200394,
+                    30.126596781356923,
+                  ),
+                },
+                {
+                  lat: -1.9365670876910166,
+                  lng: 30.13020167024439,
+                },
+              ],
+              origins: [origin],
+              travelMode: google.maps.TravelMode.DRIVING,
+            }}
+            
+          /> */}
+        </GoogleMap>
+      </>
+    );
+  }
+};
 
 export default Job;
