6e47d634c5ab6d924ceac0867f2823cb911f2193
amuhirwa <a.muhirwa@alustudent.com>
2024-06-18 14:49:08 +0200

    Fixed form

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -122,7 +122,11 @@ def schedule(request, schedule_id=None):
         if serializer.is_valid():
             schedule = serializer.save()
             save_achievement(request.user.id, 'SCHEDULE', frequency=1)
-
+            date = datetime.datetime.strftime(data['date_time'], "%d-%m-%Y")
+            notification = Notification(
+                user=request.user,
+                text=f"{request.user.first_name} {request.user.last_name} has scheduled a {data['repeat']} repeat waste collection to start on {date}."
+            )
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
     
