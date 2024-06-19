ba508cba483a669e3b37b5cadf5fa9219af7ffe3
amuhirwa <a.muhirwa@alustudent.com>
2024-06-19 13:00:36 +0200

    Fixed infinite reload when token is invalid

--- frontend/src/features/axios.js
+++ frontend/src/features/axios.js
@@ -25,7 +25,7 @@ function createAxiosInstance() {
     (error) => {
       const errorObj = error;
       if (errorObj.response?.status == 401 && store.getState().sharedData.usersLogin != []) {
-        // store.dispatch(resetStateToDefault());
+        store.dispatch(resetStateToDefault());
         window.location.href = "/auth";
       } else {
         const errorData = errorObj.response?.data;
