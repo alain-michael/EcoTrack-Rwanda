b313a26a7d264c412709f59d1768743b4a40ecde
amuhirwa <a.muhirwa@alustudent.com>
2024-06-19 12:44:08 +0200

    CORS problem fixed

--- backend/ecotrack/settings.py
+++ backend/ecotrack/settings.py
@@ -83,7 +83,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://main--eco-track-rw.netlify.app",
-    "https://eco-track-rw.netlify.app/",
+    "https://eco-track-rw.netlify.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
