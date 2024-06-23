684216bff4683e87100847ca9b07142ea4dd66e3
amuhirwa <a.muhirwa@alustudent.com>
2024-06-23 12:36:03 +0200

    test

--- backend/ecotrack/settings.py
+++ backend/ecotrack/settings.py
@@ -195,4 +195,3 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
